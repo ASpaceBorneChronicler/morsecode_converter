b48e7bc2f47d8a975674db147ef639cb94e35918
ASpaceBorneChronicler <barakawangewa70@gmail.com>
2024-08-30 00:12:39 +0300

    This is the clean version of the code.

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 def morsecode(mess):
     with open('alphabet.txt', 'r', encoding='utf-8') as file:
         x = {y.split(' ', 1)[0]: y.split(" ", 1)[1].strip('\n') for y in file.readlines()}
-        print(x)
-        morse_mess = ''
-    for letter in mess.split():
-        morse_mess += x.get(letter, '') + ' '
+
+    morse_mess = ''
+
+    for letter in mess:
+        morse_mess += x.get(letter, ' ')+' '
 
     return morse_mess
-#     pass
-#
-#
-message = input('What would you like to say?').upper()
-print(message)
-print(morsecode(message))
 
+
+message = input('What would you like to say?').upper()
+print(morsecode(message))
